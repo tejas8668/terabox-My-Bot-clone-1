38bd0e1484e36f9c491e747c13d6fd49eb5b33ed
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-12-28 21:19:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def handle_link(update: Update, context: CallbackContext) -> None:
     original_link = update.message.text
     parsed_link = urllib.parse.quote(original_link, safe='')
     modified_link = f"https://streamterabox.blogspot.com/?q={parsed_link}&m=0"
-    modified_url = f"https://demobox11.blogspot.com/2024/12/terabox.html/?q={parsed_link}"
+    modified_url = f"https://demobox11.blogspot.com/2024/12/terabox.html?q={parsed_link}"
 
     # Create a button with the modified link
     button = [
